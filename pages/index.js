661172d76e853bd0a25a555eee3ee32d54e82f3d
Noe <noe.flores@teccsup.edu.pe>
2024-11-18 18:37:42 -0500

    version2

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,16 @@ import Hero from '@/components/Home/Hero'
 import Services from '@/components/Home/Services'
 import Experience from '@/components/Home/Experience'
 import Testimonials from '@/components/Home/Testimonials'
-export default function Home () {
+import Projects from '@/components/Home/Projects'
+export default function Home() {
   return (
     <Layout title='Home' description={'Descripcionhome'}>
       <main>
-       
         <Hero />
-        <Services/>
-        <Experience/>
-     
-        <Testimonials/>
+        <Services />
+        <Experience />
+        <Projects />
+        <Testimonials />
       </main>
     </Layout>
   )
